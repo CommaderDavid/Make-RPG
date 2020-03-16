20eec307274fd2a5e5ed6303a01ca173ffbee9e6
David Boedigheimer <stealth259@gmail.com>
2020-03-16 14:38:25 -0700

    Add first test for finding player and enemy objects.

--- __tests__/RPG.test.js
+++ __tests__/RPG.test.js
@@ -1,6 +1,14 @@
 // use "debugger" to look for bugs and broken code.
 import { MainGame } from "./../src/Game.js";
+import { Player } from "./../src/player.js";
+import { Enemy } from "./../src/enemy.js";
 
 describe('MainGame', () => {
-  
+
+  test('should see a player character and enemy', () => {
+    var startGame = new MainGame("John", "Cyborg");
+
+    expect(startGame.player).toEqual("John");
+    expect(startGame.enemy).toEqual("Cyborg");
+  });
 });
