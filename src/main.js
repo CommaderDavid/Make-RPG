b5c536b30e9357397ac4a63f134bc99669e1f29e
David Boedigheimer <stealth259@gmail.com>
2020-03-18 09:37:57 -0700

    Add changes to file extract branch.

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import img from './img/enemyHP.png';
-import img from './img/playerHP.png';
 import { MainGame } from './Game.js';
 
 $(document).ready(function() {
