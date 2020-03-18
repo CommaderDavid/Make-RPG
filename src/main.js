d4778a9cfc2c3cc721ff5432349de4b62da3569c
David Boedigheimer <stealth259@gmail.com>
2020-03-18 15:22:30 -0700

    Add a disable feature to buttons to turn them on and off again.

--- src/main.js
+++ src/main.js
@@ -11,13 +11,26 @@ $(document).ready(function() {
   const en1 = new Enemy(15, 3);
   const startGame = new MainGame(player, en1);
 
-  $("#strike").click(function() {
+  $("#strike").click(function(e) {
+    e.preventDefault();
     startGame.enemyHit(en1, player.attack);
     console.log(startGame.enemy.hitPoints);
+    $("button#strike").prop("disabled", true);
+    $("button#end").prop("disabled", false);
+
+    if (en1.hitPoints >= 0) {
+      
+    }
   });
 
   $("#end").click(function() {
     startGame.playerHit(player, en1.attack);
     console.log(startGame.player.hitPoints);
+    $("button#end").prop("disabled", true);
+    $("button#strike").prop("disabled", false);
+
+    if (en1.hitPoints >= 0) {
+
+    }
   });
 });
