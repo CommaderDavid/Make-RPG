6f49db901242f21a5061764dc3444ea40e372bda
David Boedigheimer <stealth259@gmail.com>
2020-03-19 14:19:07 -0700

    Add displays for health for both enemy and player.

--- src/main.js
+++ src/main.js
@@ -11,26 +11,35 @@ $(document).ready(function() {
   const en1 = new Enemy(15, 3);
   const startGame = new MainGame(player, en1);
 
+  $("#enemyHP").append(en1.hitPoints);
+  $("#playerHP").append(player.hitPoints);
+
   $("#strike").click(function(e) {
     e.preventDefault();
     startGame.enemyHit(en1, player.attack);
     console.log(startGame.enemy.hitPoints);
+
+    $("#enemyHP").empty().append(en1.hitPoints);
+
     $("button#strike").prop("disabled", true);
     $("button#end").prop("disabled", false);
 
     if (en1.hitPoints <= 0) {
-      $(".results").append("<h3>" + "You Win!" + "</h3>");
+      $("#results").empty().append("<h3>" + "You Win!" + "</h3>");
     }
   });
 
   $("#end").click(function() {
     startGame.playerHit(player, en1.attack);
     console.log(startGame.player.hitPoints);
+
+    $("#playerHP").empty().append(player.hitPoints);
+
     $("button#end").prop("disabled", true);
     $("button#strike").prop("disabled", false);
 
-    // if (en1.hitPoints >= 0) {
-    //
-    // }
+    if (player.hitPoints <= 0) {
+      $("#results").empty().append("<h3>" + "Game Over" + "</h3>");
+    }
   });
 });
