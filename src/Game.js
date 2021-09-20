24550a29256ca9d7a90ca0d1c506b89a54a476a1
David Boedigheimer <stealth259@gmail.com>
2021-09-20 16:15:15 -0700

    Add the start of functional programing in enemy.js

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,3 @@
-import { Player } from "./player.js";
-import { Enemy } from "./enemy.js";
-
 export class MainGame {
   constructor(player, enemy) {
     this.player = player;
--- src/enemy.js
+++ src/enemy.js
@@ -6,3 +6,21 @@ export class Enemy {
     this.dead = false;
   }
 }
+
+function enemyPower(strength) {
+  return function (level) {
+    return Math.trunc(level * strength);
+  }
+}
+
+function enemyHitChance(dodge) {
+  return function (armorWeight) {
+    return Math.trunc(dodge - armorWeight);
+  }
+}
+
+function enemyTotalHP(armorHealth) {
+  return function (hitPoints) {
+    return Math.trunc(armorHealth + hitPoints);
+  }
+}
